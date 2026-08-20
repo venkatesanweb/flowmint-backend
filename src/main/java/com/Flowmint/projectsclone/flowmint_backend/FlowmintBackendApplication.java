3f543df6d64d6a9e722bd08c3acad05b86a9e53a
venkatesanweb <venkatesan.kumarsivan@gmail.com>
2026-08-20 20:41:24 +0530

    Initial Spring boot setup with supabase connection done

---
+++ src/main/java/com/Flowmint/projectsclone/flowmint_backend/FlowmintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Flowmint.projectsclone.flowmint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowmintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowmintBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Flowmint/projectsclone/flowmint_backend/FlowmintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Flowmint.projectsclone.flowmint_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowmintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
