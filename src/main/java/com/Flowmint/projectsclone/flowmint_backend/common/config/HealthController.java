fae440a9c36a4ba76fa24b392b47541b47ba9eae
venkatesanweb <venkatesan.kumarsivan@gmail.com>
2026-08-21 22:30:25 +0530

    Frontend and backend connection is resuccessful

--- src/main/java/com/Flowmint/projectsclone/flowmint_backend/common/config/HealthController.java
+++ src/main/java/com/Flowmint/projectsclone/flowmint_backend/common/config/HealthController.java
@@ -1,13 +1,17 @@
 package com.Flowmint.projectsclone.flowmint_backend.common.config;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class HealthController {
 
-    @GetMapping("/api/health")
-    public String health() {
-        return "Flowmint backend is running Scuessfuly";
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "Backend is running and connected!";
     }
+    
 }
